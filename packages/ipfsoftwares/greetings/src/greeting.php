a21c130c70d4072ba5f3f58d7770bcfcc4e5fad9
dahabu saidi <dahabusaidi@gmail.com>
2020-03-05 21:16:13 +0300

    Package setup

---
+++ packages/ipfsoftwares/greetings/src/greeting.php
@@ -0,0 +1,10 @@
+<?php
+namespace IPFSoftwares\Greetings;
+
+class Greetr
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
