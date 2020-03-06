5cbb9bef32df629425be9a326b2d20a92ec40743
dahabu saidi <dahabusaidi@gmail.com>
2020-03-06 04:09:21 +0300

    Add sample usage

---
+++ app/Http/Controllers/TestResponseControllter.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use IPFSoftwares\ResponseFormatter\Formatter;
+
+class TestResponseControllter extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = [
+            'name' => 'Saidi Mkawa',
+            'email' => 'dahabusaidi@gmail.com',
+            'phone' => '0717495198',
+            'address' => 'Bagamoyo Tanzania',
+        ];
+        $message = 'Success';
+        $metadata = [];
+        $res = [];
+
+        /** Initialize formatter object */
+        $formater = new Formatter($message, $metadata, $data, 200 );
+
+        return $formater->format();
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
