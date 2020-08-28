62a33933ec1d9a1b618e04d210f1cd50b78d78e0
dahabu saidi <dahabusaidi@gmail.com>
2020-08-28 13:32:35 +0300

    Rename file

--- app/Http/Controllers/Api/ApiSendEmailController-r.php
+++ app/Http/Controllers/Api/ApiSendEmailController-r.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use IPFSoftwares\MailSender\Mail;
+use MailSender\Mail;
 use IPFSoftwares\Greetings\Greetr;
 
 class ApiSendEmailController extends Controller
 {
     public function sendEmail(Request $request)
     {
-        $email = new Greetr();
-        $sendEmail = $email->greet('mkawa');
+        $email = new \MailSender\Mail;
+        $sendEmail = $email->sendEmail('mkawa');
         $data = [
             'data' => $request->all(),
             'email' => $sendEmail,
